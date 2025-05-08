d7e928530303030fa975d67325bbef6e6d4bdf32
Wu xi <textxin886@example.com>
2025-05-08 15:40:07 +0800

    完成

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/image/xxmLogo.png";
-const _imports_0 = "/assets/classify2.a7a768ab.jpg";
-exports._imports_0 = _imports_0$1;
-exports._imports_0$1 = _imports_0;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
